1ed879c55c50ac0c34902b5551fe810470011f59
JagathVM <17jagathvm@gmail.com>
2025-12-25 00:44:28 +0530

    Add Python variables and data types example

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello Python")
+# Python Variables and Data Types
+
+name = "Jagath"
+age = 23
+is_developer = True
+
+print("Name: ", name)
+print("Age: ", age)
+print("Is Developer: ", is_developer)
+
+
+# "text" -> string
+# 23 -> integer
+# True/False -> boolean
